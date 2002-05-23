ab31c63a0d2f986892d18ed00c3a23fe050cfafb
tron <tron>
2002-05-23 00:54:48 +0000

    Opened up correct tex2pdf modul and checked in first tools: release.sh: shell script to prepare a release of tex2pdf update-www.sh: tiny shell script to update our WWW site at BerliOS

---
+++ tex2pdf/tools/release.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+MAINNAME=tex2pdf-
+ARCHIVE_SUFFIX=.tar.gz
+TMP_DIR=$HOME/tmp
+PATH_BASE=$HOME/devel/tex2pdf
+BIN_DIR=$PATH_BASE/bin
+WWW_DIR=$PATH_BASE/www
+RELEASE_DIR=$PATH_BASE/releases
+RELEASE_ARCHIVES=$RELEASE_DIR/archives
+DATA_DIR=$RELEASE_DIR/data
+VERSION=$1
+NEW_RELEASE_DIR=$RELEASE_DIR/$MAINNAME$VERSION
+RELEASE_ARCHIVE=$RELEASE_ARCHIVES/$MAINNAME$VERSION$ARCHIVE_SUFFIX
+
+if [ -z "$VERSION" ]
+then
+ echo "Sorry. You must give a version string"
+  echo "Aborting ..."
+  exit 1
+fi
+ 
+if ! mkdir -p $RELEASE_ARCHIVES
+then
+  echo "Sorry. Could not create $RELEASE_ARCHIVES"
+  echo "Aborting ..."
+  exit 1
+fi
+
+if [ -d $NEW_RELEASE_DIR ]
+then
+  echo "$NEW_RELEASE_DIR exists. I will remove it."
+  rm -rf $NEW_RELEASE_DIR
+  rm -rf $RELEASE_ARCHIVE
+fi
+
+if ! mkdir $NEW_RELEASE_DIR
+then
+  echo "Sorry. Could not create $NEW_RELEASE_DIR"
+  echo "Aborting ..."
+  exit 1
+fi
+
+cd $BIN_DIR
+echo "********* Checking tex2pdf *********"
+cvs diff tex2pdf
+
+if ! cp -a tex2pdf $NEW_RELEASE_DIR
+then
+  echo "Sorry. Could not copy tex2pdf"
+  echo "Aborting ..."
+  exit 1
+fi
+
+cd $WWW_DIR
+echo
+echo "********* Checking changelog.html *********"
+cvs diff changelog.html
+
+if ! html2text -nobs changelog.html > $TMP_DIR/tex2pdf-CHANGELOG.tmp
+then
+  echo "Sorry. Could not convert changelog.html to CHANGELOG"
+  echo "Aborting ..."
+  exit 1
+fi
+
+if ! sed -e '/^=\+$/a \
+ ' -e "/\[.*Last modified.*\]/d " $TMP_DIR/tex2pdf-CHANGELOG.tmp > $NEW_RELEASE_DIR/CHANGELOG
+then
+  echo "Sorry. Could not adjust CHANGELOG"
+  echo "Aborting ..."
+  exit 1
+fi
+
+#echo "+++++++++ CHANGELOG ++++++++++"
+#cat $NEW_RELEASE_DIR/CHANGELOG
+
+echo
+echo "********* Checking lyx-howto.htm *********"
+cvs diff lyx-howto.htm
+
+if ! html2text -nobs lyx-howto.htm > $NEW_RELEASE_DIR/README
+then
+  echo "Sorry. Could not generate README"
+  echo "Aborting ..."
+  exit 1
+fi
+
+#echo "+++++++++ README ++++++++++"
+#cat $NEW_RELEASE_DIR/README
+
+cd $RELEASE_DIR
+if ! tar cfz $RELEASE_ARCHIVE $MAINNAME$VERSION
+then
+  echo "Sorry. Could not generate archive $MAINNAME$VERSION$ARCHIVE_SUFFIX"
+  echo "Aborting ..."
+  exit 1
+fi
+
+echo +++++++++++++ RELEASE NOTE ++++++++++
+cat $DATA_DIR/release_note.html
+
+echo +++++++++++++ CHANGELOG NOTE ++++++++++
+cat $DATA_DIR/changelog_note.html
+
+echo
+echo "Archive is $RELEASE_ARCHIVES/$MAINNAME$VERSION$ARCHIVE_SUFFIX"
+echo "Now upload to download.berlios.de/incoming/"
+
+echo "Good luck!"
+  
---
+++ tex2pdf/tools/update-www.sh
@@ -0,0 +1,4 @@
+scp changelog.html index.html lyx-howto.htm help.txt tron@shell.berlios.de:/home/groups/tex2pdf/htdocs/
+#scp index.html tron@tex2pdf.berlios.de:/home/groups/tex2pdf/htdocs/
+#scp lyx-howto.htm tron@tex2pdf.berlios.de:/home/groups/tex2pdf/htdocs/
+#scp help.txt tron@tex2pdf.berlios.de:/home/groups/tex2pdf/htdocs/
