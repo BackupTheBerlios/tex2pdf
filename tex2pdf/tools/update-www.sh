47ee63eb4dd937fd4b5ae4a83240a2a38228bbd4
tron <tron>
2002-06-05 10:18:37 +0000

    made it more generic

--- tex2pdf/tools/update-www.sh
+++ tex2pdf/tools/update-www.sh
@@ -1,4 +1,17 @@
-scp changelog.html index.html lyx-howto.htm help.txt tron@shell.berlios.de:/home/groups/tex2pdf/htdocs/
-#scp index.html tron@tex2pdf.berlios.de:/home/groups/tex2pdf/htdocs/
-#scp lyx-howto.htm tron@tex2pdf.berlios.de:/home/groups/tex2pdf/htdocs/
-#scp help.txt tron@tex2pdf.berlios.de:/home/groups/tex2pdf/htdocs/
+#!/bin/bash
+
+LOCAL_WWW_DIR=$HOME/devel/tex2pdf/www/
+REMOTE_WWW_DIR=/home/groups/tex2pdf/htdocs/
+REMOTE_HOST=shell.berlios.de
+REMOTE_USER=$USER
+
+if [ $@ = 0 ]
+then
+   FILES="changelog.html index.html lyx-howto.htm help.txt"
+else
+   FILES="$*"
+fi
+
+cd $LOCAL_WWW_DIR
+
+scp $FILES $REMOTE_USER@$REMOTE_HOST:$REMOTE_WWW_DIR
